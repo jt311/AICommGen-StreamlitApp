79b714265a08d754c286c4d8d9969a5d2068621a
JoshTakchi <josh.takchi@gmail.com>
2024-07-05 22:02:10 +1000

    v1

--- main.py
+++ main.py
@@ -3,19 +3,32 @@ from output import generateCommands_JSONStr, generateCommands_JSONToDict
 
 st.title("Cisco IOS - Command Generator")
 st.subheader("What would you like to do on the Cisco device?")
-user_input = st.text_input("EG: I want to configure an ip address on interface fa0/0")
-
 
 # Streamlit App
-if st.button("Generate Commands") and user_input:
-    with st.spinner("Generating commands..."):
-        json_str_res = generateCommands_JSONStr(user_input=user_input)
-    if (json_str_res != "Invalid Input"):
-        commands_object = generateCommands_JSONToDict(json_str=json_str_res)
-        comm_flow_str = f"{commands_object['flow_name']}"
-        st.write(comm_flow_str)
-        for elem in commands_object['commands']:
-            comm_str = f"{elem['description']}: {elem['command']}"
-            st.write(comm_str)
-    else: 
-        st.write(json_str_res)
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if user_input := st.chat_input("I want to..."):
+    st.session_state.messages.append({"role": "user", "content": user_input})
+    with st.chat_message("user"):
+        st.markdown(user_input)
+
+    json_str_res = generateCommands_JSONStr(user_input=user_input)
+    if (json_str_res != "Invalid Data"):
+        commands_object = generateCommands_JSONToDict(json_str_res)
+        comm_flow_name = commands_object['flow_name'] + ":"
+        comm_arr = [f"{idx+1}. {comm_object['description']}: {comm_object['command']}" 
+                    for idx, comm_object in enumerate(commands_object['commands'])]
+        
+        llm_response = "\n".join([comm_flow_name, *comm_arr])
+    else:
+        llm_response = json_str_res
+     
+    with st.chat_message("assistant"):
+        st.markdown(f"{llm_response}")
+        
+    st.session_state.messages.append({"role": "assistant", "content": llm_response})
