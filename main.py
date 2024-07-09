5d4cd03bbc7de20010195bd9cd4e27286d836520
JoshTakchi <josh.takchi@gmail.com>
2024-07-09 21:07:24 +1000

    expander

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import streamlit as st
 from output import generateCommands_JSONStr, generateCommands_JSONToDict
 
-st.title("Cisco IOS - Command Generator")
-st.subheader("What would you like to do on the Cisco device?")
+st.title("Cisco IOS - Command Generator :link:")
+st.subheader(":green[What would you like to do on the Cisco device?]")
+with st.expander("See examples"):
+    with st.container():
+        st.markdown("""**:green[I want to]** configure an ip address on interface FastEthernet0/0.
+                    Then, I want to run hsrp on this interface with virtual ip 10.10.10.1""")
+        
+        st.markdown("""**:green[I want to]** configure ospf on R1.
+                    The network is 10.0.0.0, the wildcard mask is 0.0.255.255 and the area is 1""")
+        
+        st.markdown("""**:green[I want to]** view the routing table on R3""")
 
 # Streamlit App
 if "messages" not in st.session_state:
@@ -30,5 +39,6 @@ if user_input := st.chat_input("I want to..."):
      
     with st.chat_message("assistant"):
         st.markdown(f"{llm_response}")
+        # add Copy Commands button to Pop-Up
         
     st.session_state.messages.append({"role": "assistant", "content": llm_response})
